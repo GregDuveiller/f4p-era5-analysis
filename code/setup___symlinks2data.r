19bcdeca97754ce6b9a243cef92a0f9312c1aec7
GregDuveiller <gduveiller@gmail.com>
2021-02-12 16:56:33 +0100

    Restructure code organizatino

--- code/setup___symlinks2data.r
+++ code/setup___symlinks2data.r
@@ -2,8 +2,8 @@
 
 # clim data
 data_path <- '/Users/greg/work/data/external_datasets/climate_zones/Map_KG-Global/'
-file.symlink(to = 'data/climate_zones', from = data_path)
+file.symlink(to = 'data/input_data/climate_zones', from = data_path)
 
 # vctr data
 data_path <- '/Volumes/home/work/data/external_datasets/WorldVector/'
-file.symlink(to = 'data/world_vectors', from = data_path)
+file.symlink(to = 'data/input_data/world_vectors', from = data_path)
