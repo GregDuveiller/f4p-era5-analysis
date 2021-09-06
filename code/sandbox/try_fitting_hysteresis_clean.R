ef93d2e64611dfc96b5fa3b728493614be1aa063
GregDuveiller <gduveiller@gmail.com>
2021-09-06 18:12:33 +0200

    clean-up and automate plots

--- code/sandbox/try_fitting_hysteresis_clean.R
+++ code/sandbox/try_fitting_hysteresis_clean.R
@@ -3,13 +3,7 @@ require(ggplot2)
 require(dplyr)
 require(viridis)
 
-# some graphic param...
-lgd <- theme(legend.position = 'left',
-             legend.key.height = unit(3, units = 'cm'),
-             panel.grid = element_blank(),
-             strip.text = element_text(size = 6))
-# set-up colour palette
-cols <- c(viridis(n = 6), magma(n = 6)[6:1])
+
 
 # load/prepare dataframe with data...
 load('data/inter_data/df_single_var_agreement/df_single_var_agr_tmp_LAI_cleaner.RData')
@@ -27,10 +21,26 @@ df_all <- left_join(by = by_vctr, suffix = c("_LAI", "_LST"),
   mutate(year = as.numeric(format(time,'%Y'))) %>%
   mutate(monthS = as.numeric(format(time, '%m')))
 
+
+
+# t2.bin <- 0.26
+# sm.bin <- 0.2
+
+fig.format <- 'png'
+fig.path <- 'results/hysteresis'
+dir.create(fig.path)
+
+# define function to make plots per 2d bin
+fit.hyst.plot <- function(t2.bin.num, sm.bin.num){
+
 # filter here for a section of the climspace
 df <- df_all %>%
-  filter(t2.clim.bin == -2) %>%  # <-  need to change these accordingly ///
-  filter(sm.clim.bin == 0.52)    # <-  need to change these accordingly /// 
+  filter(t2.clim.bin == levels(df_all$t2.clim.bin)[t2.bin.num]) %>%  
+  filter(sm.clim.bin == levels(df_all$sm.clim.bin)[sm.bin.num])    
+
+if(dim(df)[1] == 0){
+  # print('No data for this bin')
+  return(g = NULL)}
 
 # calculate monthly averages
 df_m <- df %>%
@@ -48,18 +58,20 @@ t <- c(df$monthS - 12, df$monthS, df$monthS + 12)
 per <- 12
   
 # second harmonic fits
-tyfit <- lm(y ~ sin(2*pi/per*t)+cos(2*pi/per*t)+sin(4*pi/per*t)+cos(4*pi/per*t))
-txfit <- lm(x ~ sin(2*pi/per*t)+cos(2*pi/per*t)+sin(4*pi/per*t)+cos(4*pi/per*t))
+tyfit2 <- lm(y ~ sin(2*pi/per*t)+cos(2*pi/per*t)+sin(4*pi/per*t)+cos(4*pi/per*t))
+txfit2 <- lm(x ~ sin(2*pi/per*t)+cos(2*pi/per*t)+sin(4*pi/per*t)+cos(4*pi/per*t))
 
 lmfit <- lm(y ~ x)
 
 # more complicated fits... 
-tyfit <- lm(y ~ sin(2*pi/per*t)+cos(2*pi/per*t)+sin(4*pi/per*t)+cos(4*pi/per*t)+sin(6*pi/per*t)+cos(6*pi/per*t))
-txfit <- lm(x ~ sin(2*pi/per*t)+cos(2*pi/per*t)+sin(4*pi/per*t)+cos(4*pi/per*t)+sin(6*pi/per*t)+cos(6*pi/per*t))
+tyfit3 <- lm(y ~ sin(2*pi/per*t)+cos(2*pi/per*t)+sin(4*pi/per*t)+cos(4*pi/per*t)+sin(6*pi/per*t)+cos(6*pi/per*t))
+txfit3 <- lm(x ~ sin(2*pi/per*t)+cos(2*pi/per*t)+sin(4*pi/per*t)+cos(4*pi/per*t)+sin(6*pi/per*t)+cos(6*pi/per*t))
 
-df_s <- data.frame(t = seq(0,12,0.1))
-df_s$y <- predict.lm(tyfit, df_s)
-df_s$x <- predict.lm(txfit, df_s)
+df_s <- data.frame(t = seq(0.5,12.5,0.1))
+df_s$y3 <- predict.lm(tyfit3, df_s)
+df_s$x3 <- predict.lm(txfit3, df_s)
+df_s$y2 <- predict.lm(tyfit2, df_s)
+df_s$x2 <- predict.lm(txfit2, df_s)
 df_s$monthS <- df_s$t
 
 
@@ -78,19 +90,35 @@ df_p <- data.frame(
 
 #### plotting ####
 
+# some graphic param...
+lgd <- theme(legend.position = 'left',
+             legend.key.height = unit(3, units = 'cm'),
+             panel.grid = element_blank(),
+             strip.text = element_text(size = 6))
+# set-up colour palette
+cols <- c(viridis(n = 6), magma(n = 6)[6:1])
+
+
 gry1 <- 'grey60'  # <-- the axes
 gry2 <- 'grey30'  # <-- the edges of the points
 gry3 <- 'grey45'  # <-- the smoothed path
 
+fig.format <- 'png'
+
+xrange <- range(df_all$x)
+yrange <- range(df_all$y)
+
+
 # overall plot of the situation
-ggplot(df) + 
+gxy <- ggplot(df) + 
   geom_hline(yintercept = 0, colour = gry1) +
   geom_vline(xintercept = 0, colour = gry1) +
   geom_point(aes(x = x, y = y, fill = monthS),
              colour = gry2, stroke = 0.4, shape = 21, size = 2) +
   geom_abline(slope = lmfit$coefficients[2], intercept = lmfit$coefficients[1],
               colour = gry3, linetype = 'dashed') +
-  geom_path(data = df_s, aes(x = x, y = y), colour = gry3, size = 2) +
+  geom_path(data = df_s, aes(x = x2, y = y2), colour = gry1, size = 1.5) +
+  geom_path(data = df_s, aes(x = x3, y = y3), colour = gry3, size = 2) +
   geom_errorbar(data = df_m, aes(x = x_mu, ymin = y_mu - y_sd, ymax = y_mu + y_sd),
                 colour = gry2,  width = 0.02) +
   geom_errorbarh(data = df_m, aes(y = y_mu, xmin = x_mu - x_sd, xmax = x_mu + x_sd),
@@ -100,38 +128,64 @@ ggplot(df) +
   scale_fill_gradientn('', colours = cols, breaks = 1:12, labels = month.abb) +
   scale_y_continuous(paste('Bias in', varname, '(ERA - obs)')) +
   scale_x_continuous('Bias in LAI (ERA - obs)') +
-  ggtitle(label = paste0('Hysteresis pattern between bias in LAI and bias in ', varname),
-          subtitle = paste0('Slope = ', format(df_p$slope, digits = 4), ' | ',
-                            'Area = ', format(df_p$area, digits = 4))) + 
+  coord_cartesian(xlim = xrange, ylim = yrange) +
+  ggtitle(label = paste0('Hysteresis pattern between bias in LAI and bias in ', varname), 
+          subtitle = paste0('Centre of T2 bin: ', levels(df_all$t2.clim.bin)[t2.bin.num], '   ',
+                            'Centre of SM bin: ', levels(df_all$sm.clim.bin)[sm.bin.num])) + 
+          # subtitle = paste0('Slope = ', format(df_p$slope, digits = 4), ' | ',
+          #                   'Area = ', format(df_p$area, digits = 4))) + 
   lgd
 
 
 # make plot of decomposed sequences
 
-ggplot(df, aes(x = monthS, fill = monthS)) + 
+gx <- ggplot(df, aes(x = monthS, fill = monthS)) + 
   geom_hline(yintercept = 0, colour = gry1) +
-  geom_line(data = df_s, aes(y = x), 
-            colour = gry3, size = 2) +
+  geom_line(data = df_s, aes(y = x2), colour = gry1, size = 1.5) +
+  geom_line(data = df_s, aes(y = x3), colour = gry3, size = 2) +
   geom_point(aes(y = x), 
              colour = gry2, stroke = 0.3, shape = 21, size = 2) +
   geom_errorbar(data = df_m, aes(ymin = x_mu - x_sd, ymax = x_mu + x_sd),
                 colour = gry2,  width = 0.2) +
   geom_point(data = df_m, aes(y = x_mu), 
              colour = gry2, stroke = 0.5, shape = 21, size = 4) +
-  scale_fill_gradientn('', colours = cols, breaks = 1:12, labels = month.abb) +
-  ggtitle(label = paste0('Smoothed temporal pattern for the LAI bias axis')) + 
+  scale_fill_gradientn('', colours = cols, breaks = 1:12, labels = month.abb, guide = "none") +
+  scale_y_continuous(paste('Bias in', 'LAI', '(ERA - obs)')) +
+  scale_x_continuous('Month of the year (seasonally synched to NH)', breaks = 1:12, 
+                     labels = month.abb, expand = c(0, 0), limits = c(0.5,12.5)) +
+  #ggtitle(label = paste0('Smoothed temporal pattern for the LAI bias axis')) + 
   lgd
 
-ggplot(df, aes(x = monthS, fill = monthS)) + 
+gy <- ggplot(df, aes(x = monthS, fill = monthS)) + 
   geom_hline(yintercept = 0, colour = gry1) +
-  geom_line(data = df_s, aes(y = y), 
-            colour = gry3, size = 2) +
+  geom_line(data = df_s, aes(y = y2), colour = gry1, size = 1.5) +
+  geom_line(data = df_s, aes(y = y3), colour = gry3, size = 2) +
   geom_point(aes(y = y), 
              colour = gry2, stroke = 0.3, shape = 21, size = 2) +
   geom_errorbar(data = df_m, aes(ymin = y_mu - y_sd, ymax = y_mu + y_sd),
                 colour = gry2,  width = 0.2) +
   geom_point(data = df_m, aes(y = y_mu), 
              colour = gry2, stroke = 0.5, shape = 21, size = 4) +
-  scale_fill_gradientn('', colours = cols, breaks = 1:12, labels = month.abb) +
-  ggtitle(label = paste0('Smoothed temporal pattern for the ', varname, ' bias axis')) + 
+  scale_fill_gradientn('', colours = cols, breaks = 1:12, labels = month.abb, guide = "none") +
+  scale_y_continuous(paste('Bias in', varname, '(ERA - obs)')) +
+  scale_x_continuous('Month of the year (seasonally synched to NH)', breaks = 1:12, 
+                     labels = month.abb, expand = c(0, 0), limits = c(0.5,12.5)) +
+  #ggtitle(label = paste0('Smoothed temporal pattern for the ', varname, ' bias axis')) + 
   lgd
+
+require(patchwork)
+
+g <- gxy + gx / gy
+ggsave(filename = paste0('hyst_LSTvsLAI_', sprintf('SMbin%02d',sm.bin.num), '_', 
+                         sprintf('TMbin%02d',t2.bin.num),
+                         '.', fig.format),
+       path = fig.path, width = 16, height = 9)
+
+ return(g)
+}
+
+for(i in 1:length(levels(df_all$t2.clim.bin))){
+  for(j in 1:length(levels(df_all$sm.clim.bin))){
+    fit.hyst.plot(i, j)
+  }
+}
