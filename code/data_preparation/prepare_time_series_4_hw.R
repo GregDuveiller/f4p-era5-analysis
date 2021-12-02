5f64a16f6c6037de6697bda702b1de86c80183ef
GregDuveiller <gduveiller@gmail.com>
2021-12-02 18:13:07 +0100

    improve HW ts gathering"  ' '
    '

--- code/data_preparation/prepare_time_series_4_hw.R
+++ code/data_preparation/prepare_time_series_4_hw.R
@@ -22,41 +22,58 @@ df_hw <- data.frame(row.names = c("hw2003", "hw2010", "hw2018"),
                     year = c(2003, 2010, 2018),
                     month = c(8, 7, 7))
 
-hw_bbox <- hw_polygons %>% filter(hw == 'hw2003') %>% st_bbox()
-
-i <- 1
-
-# for (i in 1:length(var_list)){
-variable_i <- var_list[i] ; print(variable_i)
-input_file <- paste0('df_comb___',variable_i,'.RData')
-load(paste0(input_dir, input_file))
 
 
-df_subsetHW <- df_comb %>%
-  filter(x >= hw_bbox$xmin & x <= hw_bbox$xmax) %>%
-  filter(y >= hw_bbox$ymin & y <= hw_bbox$ymax)
 
-rm(df_comb)
+get_hw_spatial_average <- function(hwname, varname){
 
-
-mutate(time = as.Date(paste(year, month, '15', sep = '-'), '%Y-%M-%d')) %>%
-  #filter(x == -125.125, y == 72.375) %>%  #  <---- need to replace this... 
-
-%>%
-    group_by(time) %>%
-    summarise(obs = mean(obs, na.rm = T), sim = mean(sim, na.rm = T))
-
+  load(paste0(input_dir, 'df_comb___', varname, '.RData'))  # <--- df_comb
     
-ts_all <- ts %>% 
-  group_by(month) %>%
-  summarise(clim_obs = mean(obs),
-            clim_sim = mean(sim)) %>%
-  left_join(ts %>% filter(year %in% c(2003, 2010, 2018)), by = "month") %>%
+  hw_bbox <- hw_polygons %>% filter(hw == hwname) %>% st_bbox()
+
+  df_subsetHW <- df_comb %>%
+    filter(x >= hw_bbox$xmin & x <= hw_bbox$xmax) %>%
+    filter(y >= hw_bbox$ymin & y <= hw_bbox$ymax)
+  
+  rm(df_comb)
+  
+  df_subset_ts <- df_subsetHW  %>%
+    group_by(year, month) %>%
+    summarise(obs = mean(obs, na.rm = T), sim = mean(sim, na.rm = T)) %>%
+    mutate(time = as.Date(paste(year, month, '15', sep = '-'), '%Y-%M-%d'),
+           varname = varname,
+           hwname = hwname)
+  
+  return(df_subset_ts)
+}
+
+df_subset_1 <- get_hw_spatial_average(hwname = 'hw2003', varname = 'LAI')
+df_subset_2 <- get_hw_spatial_average(hwname = 'hw2010', varname = 'LAI')
+df_subset_3 <- get_hw_spatial_average(hwname = 'hw2018', varname = 'LAI')
+
+df_subset_ts <- bind_rows(df_subset_1, df_subset_2, df_subset_3)
+
+df_subset_ts <- bind_rows(
+  get_hw_spatial_average(hwname = 'hw2003', varname = 'LST'),
+  get_hw_spatial_average(hwname = 'hw2010', varname = 'LST'),
+  get_hw_spatial_average(hwname = 'hw2018', varname = 'LST')
+)
+
+
+
+hw_clim_ts <- df_subset_ts %>% 
+  group_by(month, hwname) %>%
+  summarise(clim_obs = mean(obs, na.rm = T),
+            clim_sim = mean(sim, na.rm = T))
+
+
+
+ts_all <- df_subset_ts %>% 
+  filter(hwname == paste0('hw', year)) %>%
+  left_join(hw_clim_ts, by = c('month', 'hwname')) %>%
   rename(year_of_hw = year) %>%
   left_join(data.frame(year_of_hw = c(2003, 2010, 2018), 
                        month_of_hw = c(7, 8, 8)), by = "year_of_hw")
-
-
 
 
 
@@ -65,8 +82,9 @@ ggplot(ts_all, aes(x = month)) +
   geom_line(aes(y = clim_obs)) +
   geom_line(aes(y = clim_sim), linetype = 'dashed') +
   geom_line(aes(y = obs, colour = factor(year_of_hw)), show.legend = F) +
+  geom_line(aes(y = sim, colour = factor(year_of_hw)), show.legend = F, linetype = 'dashed') +
   facet_grid(year_of_hw~.) +
-  scale_x_continuous(expand = c(0,0))
+  scale_x_continuous(expand = c(0,0)) 
 
 
 
