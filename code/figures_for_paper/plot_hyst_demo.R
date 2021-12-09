e70d2a44e567e489d638b77c2eaa54f2cb75e18c
GregDuveiller <gduveiller@gmail.com>
2021-12-09 13:44:08 +0100

    change the colour of the path

--- code/figures_for_paper/plot_hyst_demo.R
+++ code/figures_for_paper/plot_hyst_demo.R
@@ -23,9 +23,9 @@ load('data/figures_for_paper/hysteresis_data_ready4fig.RData')
 # define different cases to showcase
 case_list <- list(
   A = list(t2.bin.val = "26", sm.bin.val = "0.24",
-           case_longname = "T2M between 24 and 28 | SM between 0.22 and 0.26 "),
+           case_longname = "24 < T2M <= 28 | 0.22 < SM <= 0.26 "),
   B = list(t2.bin.val =  "6", sm.bin.val = "0.44",
-           case_longname = "T2M between 4 and 8 | SM between 0.42 and 0.46"))
+           case_longname = "4 < T2M <= 8 | 0.42 < SM <=0.46"))
 
 # filter out
 df_r <- bind_rows(
@@ -68,7 +68,7 @@ plot.case <- function(caseID, varname){
 
 # some graphic param...
 lgd <- theme(legend.position = 'left',
-             legend.key.height = unit(3, units = 'cm'),
+             legend.key.height = unit(2.5, units = 'cm'),
              panel.grid = element_blank(),
              strip.text = element_text(size = 6))
 
@@ -85,10 +85,10 @@ gry3 <- 'grey45'  # <-- the smoothed path
 g <- ggplot(df_r %>% filter(case == caseID)) + 
   geom_hline(yintercept = 0, colour = gry1) +
   geom_vline(xintercept = 0, colour = gry1) +
+  geom_path(data = df_s %>% filter(case == caseID), 
+            aes(x = x, y = y, colour = t), size = 2, show.legend = F) +
   geom_point(aes(x = x, y = y, fill = monthS),
              colour = gry2, stroke = 0.4, shape = 21, size = 2) +
-  geom_path(data = df_s %>% filter(case == caseID), 
-            aes(x = x, y = y), colour = gry3, size = 2) +
   geom_errorbar(data = df_m %>% filter(case == caseID), 
                 aes(x = x_mu, ymin = y_mu - y_sd, ymax = y_mu + y_sd),
                 colour = gry2,  width = ebarwidth) +
@@ -99,6 +99,7 @@ g <- ggplot(df_r %>% filter(case == caseID)) +
              aes(x = x_mu, y = y_mu, fill = monthS),
              colour = gry2, stroke = 0.3, shape = 21, size = 4) +
   scale_fill_gradientn('', colours = cols, breaks = 1:12, labels = month.abb) +
+  scale_colour_gradientn('', colours = cols, breaks = 1:12, labels = month.abb) +
   scale_y_continuous(paste('Bias in', varname, '(ERA - obs)')) +
   scale_x_continuous('Bias in LAI (ERA - obs)') +
   ggtitle(label = paste0('Hysteresis pattern between bias in LAI and bias in ', varname), 
@@ -123,11 +124,11 @@ dir.create(path = fig.path, recursive = T, showWarnings = F)
 fig.name <- 'hystereris_demo'
 
 g <- g1 + g2 + plot_layout(guides = "collect") & theme(legend.position = 'left')
-ggsave(filename = paste0(fig.name, '.', fig.format),
-       path = fig.path, width = 14, height = 7)
+ggsave(filename = paste0(fig.name, '.', fig.fmt),
+       path = fig.path, width = 12, height = 6)
  
  # g <- g1 / g2 + plot_layout(guides = "collect") & theme(legend.position = 'left')
-# ggsave(filename = paste0(fig.name, '.', fig.format),
+# ggsave(filename = paste0(fig.name, '.', fig.fmt),
 #        path = fig.path, width = 8, height = 14)
 
 
