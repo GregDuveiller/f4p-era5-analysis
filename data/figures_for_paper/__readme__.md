179bbf41a453dffc8a570afe4707c4d761040971
GregDuveiller <gduveiller@gmail.com>
2021-12-14 17:06:47 +0100

    change setup script

--- data/figures_for_paper/__readme__.md
+++
@@ -1,2 +0,0 @@
-# data/figures_for_paper
-This folder contains the data needed to make the figures of the paper.
--- setup_data_folder_structure.R
+++ setup_data_folder_structure.R
@@ -8,18 +8,54 @@
 # ---------------------------------------------------------------------------- #
 
 
+#### Location of necessary input files ####
+# [this will be specific to a given local set-up]
+# [this could be automatized, or separated into another user-specific file]
 
-#### setup directory structure for the data ####
+# NATURAL EARTH VECTOR FILES #
+dpath_vectors <- '/Users/gduveiller/work/data/external_datasets/natural_earth_data/'
+
+# Preprocessed R dataframes with ERA5 data
+dpath_r_dataframes <- '/Users/gduveiller/work/data/internal_datasets/f4p-era5-analysis/'
+
+# Koppen-Geiger climate zones
+dpath_climate_zones <- '/Users/gduveiller/work/data/external_datasets/climate_zones/KG/'
+
+
+
+#### Input data directory setup ####
 
 # folder for input data, which can be downloaded from elsewhere
 dir.create('data/input_data', recursive = T)
-# folder for "intermeadiate" data, which can be regenerated from code if needed
+
+# setup symlink for shapefile vector data 
+file.symlink(to = 'data/input_data/world_vectors', from = dpath_vectors)
+# setup symlink to pre-processed R dataframes of ERA5 and satellite data 
+file.symlink(to = 'data/input_data/r_data_frames', from = dpath_r_dataframes)
+# setup link to KG climate zones...
+file.symlink(to = 'data/input_data/climate_zones', from = dpath_climate_zones)
+
+
+#### Intermediate data directory setup ####
+
+# folder for "intermediate" data, which can be regenerated from code if needed
 dir.create('data/inter_data', recursive = T)
+
+# prepared ancillary info, in this case 2 types of climate zones
+dir.create('data/inter_data/ancillary_info', recursive = T)
+# this one will contain R dataframes where obs and sims (ERA5) are combined
+dir.create('data/inter_data/df_comb_obs_vs_sim', recursive = T)
+# this one will contain R dataframeshigher order summaries of the previous ones
+dir.create('data/inter_data/df_single_var_summaries', recursive = T)
+
+
+#### Final data directory setup ####
+
 # folder for final data, which is worth saving in a permanent curated repo...
 dir.create('data/final_data', recursive = T)
 
+# here we put the essential data needed to reproduce the figures in the final paper
+dir.create('data/final_data/figures_for_paper', recursive = T)
 
-#### setup symlinks for input data ####
-data_path <- '/Users/gduveiller/work/data/external_datasets/natural_earth_data/'
-file.symlink(to = 'data/input_data/vector_layers', from = data_path)
+
 
