8f990ab523152dcfe4b5426e6ad12f26f786ec66
Mark Pickering <markpickering1@hotmail.co.uk>
2021-12-17 09:59:13 +0100

    update paths of combine dfs to flat structure

--- code/combine_sim_with_obs/harvest___CombDFs___LAI.R
+++ code/combine_sim_with_obs/harvest___CombDFs___LAI.R
@@ -11,11 +11,10 @@ library(dplyr)
 library(tidyr)
 
 # input data
-dat_path <- '/media/mark/HD/Mark/Mark_COPERNICUS/data/COPERNICUSII_V3'
-#output data
+dpath_r_dataframes <- 'data/input_data/r_data_frames' 
 #output data
 out_path <- 'data/inter_data/df_comb_obs_vs_sim'
-dir.create(path = out_path, recursive = T, showWarnings = F) # MP
+dir.create(path = out_path, recursive = T, showWarnings = F)
 source('code/combine_sim_with_obs/function___get_df_comb_lai.R')
 
 spres <- '025'
@@ -26,7 +25,7 @@ get_df_comb_lai(
   spres = spres,
   src_obs = 'theia',
   src_sim = 'ERA5sl',
-  path_obs = paste0(dat_path,'/theia/dataframe_res025'),
-  path_sim = paste0(dat_path,'/CDS/LAI_ERA5sl/dataframe_res025'), 
+  path_obs = paste0(dpath_r_dataframes,''), # path extension to dataframes within dataframe dir, e.g: /theia/dataframe_res025
+  path_sim = paste0(dpath_r_dataframes,''), # path extension to dataframes within dataframe dir, e.g: /CDS/LAI_ERA5sl/dataframe_res025
   out_path = out_path
 )
--- code/combine_sim_with_obs/harvest___CombDFs___LST.R
+++ code/combine_sim_with_obs/harvest___CombDFs___LST.R
@@ -1,10 +1,20 @@
-### prototype for a matching script (between RS and ReAnalysis)
+#!/usr/local/bin/Rscript
+# ---------------------------------------------------------------------------- #
+# #### harvest___CombDFs___LAI.R ####
+# ---------------------------------------------------------------------------- #
+# Purpose: match the RS and ERA5 reanalysis data sources in single dataframe
+# Project: f4p-era5-analysis
+# Authors: G.Duveiller, M.Pickering
+# ---------------------------------------------------------------------------- #
 
 library(dplyr)
 library(tidyr)
 
-dat_path <- '/media/mark/HD/Mark/Mark_COPERNICUS/data/COPERNICUSII_V3'
-out_path <- '/media/mark/HD/Mark/Mark_COPERNICUS/data/COPERNICUSII_V3/greg_workspace'
+# input data
+dpath_r_dataframes <- 'data/input_data/r_data_frames' 
+#output data
+out_path <- 'data/inter_data/df_comb_obs_vs_sim'
+dir.create(path = out_path, recursive = T, showWarnings = F) 
 source('code/combine_sim_with_obs/function___get_df_comb_std.R')
 
 spres <- '025'
@@ -16,8 +26,8 @@ get_df_comb_std(
   src_obs = 'GEE',
   src_sim = 'ERA5l',
   target_var_sim = 'SKT',
-  path_obs = paste0(dat_path,'/GEE/LST_MODIS/dataframe_res', spres),
-  path_sim = paste0(dat_path,'/CDS/SKT_ERA5l/dataframe_res', spres),
+  path_obs = paste0(dpath_r_dataframes,''), # path extension to dataframes within dataframe dir , e.g: '/GEE/LST_MODIS/dataframe_res025'
+  path_sim = paste0(dpath_r_dataframes,''), # path extension to dataframes within dataframe dir , e.g: '/CDS/SKT_ERA5l/dataframe_res025'
   varDFname_obs = 'LST_max5_mean',
   varDFname_sim = 'skt_top5avg',
   out_path = out_path
